05ea8342212c630541403ec52dc397ad5fe5c151
Antonio Donato <donant@gmail.com>
2021-04-24 21:56:58 +0200

    Initial commit

---
+++ exec.py
@@ -0,0 +1,20 @@
+import re
+
+class stringa:
+    def __init__(self):
+        self.somma = 0
+        self.parametri = ""
+
+    def add(self, parametri):
+        lista_parametri = []
+        self.parametri = parametri
+
+        if self.parametri != " ":
+            if self.parametri.endswith("\n"):
+                return 0
+            lista_parametri = re.split(r",|\n", self.parametri)
+
+        for elemento in lista_parametri:
+            self.somma += int(elemento)
+
+        return self.somma
